7aa7dd0499088a8fd99f3d3434c6fcbdff4ed851
madiepev <madiepev@microsoft.com>
2022-05-25 20:36:54 +0200

    update

--- src/model/mlflow-autolog.py
+++ src/model/mlflow-autolog.py
@@ -2,75 +2,57 @@
 import mlflow
 import argparse
 import glob
-import pickle
-from pathlib import Path
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
-
 # define functions
 def main(args):
     # enable auto logging
     mlflow.autolog()
-
+    
     # read data
     data_path = args.training_data
     all_files = glob.glob(data_path + "/*.csv")
     df = pd.concat((pd.read_csv(f) for f in all_files), sort=False)
-
+    
     # process data
     X_train, X_test, y_train, y_test = process_data(df)
 
     # train model
     model = train_model(args.reg_rate, X_train, X_test, y_train, y_test)
- 
-    # Output the model
-    pickle.dump(model, open((Path(args.model_output) / "model.sav"), "wb"))
-
-
+    
 def process_data(df):
     # split dataframe into X and y
-    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure',
-               'TricepsThickness', 'SerumInsulin', 'BMI',
-               'DiabetesPedigree', 'Age']].values, df['Diabetic'].values
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
 
     # train/test split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30,
-                                                        random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
 
     # return splits and encoder
     return X_train, X_test, y_train, y_test
 
-
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
-    model = LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train,
-                                                                     y_train)
+    model = LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
 
     # return model
     return model
-
 
 def parse_args():
     # setup arg parser
     parser = argparse.ArgumentParser()
 
     # add arguments
-    parser.add_argument("--training_data", type=str,
-                        help="Path to training data")
-    parser.add_argument("--reg_rate", dest='reg_rate',
-                        type=float, default=0.01)
-    parser.add_argument("--model_output", type=str,
-                        help="Path of output model")
+    parser.add_argument("--training_data", dest='training_data', type=str)
+    parser.add_argument("--reg_rate", dest='reg_rate', type=float, default=0.01)
 
     # parse args
     args = parser.parse_args()
 
     # return args
     return args
-
 
 # run script
 if __name__ == "__main__":
